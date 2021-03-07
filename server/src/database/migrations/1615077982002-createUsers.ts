06d8abd003dd267c7bf624dab89189c065470b35
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-03-06 21:58:59 -0300

    add createUser migration init

---
+++ server/src/database/migrations/1615077982002-createUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createUsers1615077982002 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
