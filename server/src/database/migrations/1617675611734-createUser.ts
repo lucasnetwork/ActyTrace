43843886f71418cab27f705aa23c8c9f83a74ce8
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-04-09 22:08:23 -0300

    add createUsers migrations

---
+++ server/src/database/migrations/1617675611734-createUser.ts
@@ -0,0 +1,39 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createUser1617675611734 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+         await queryRunner.createTable(new Table({
+            name:"users",
+            columns:[
+                {
+                    name:"id",
+                    type:"int",
+                    isPrimary:true,
+                },
+                {
+                    name:"email",
+                    type:"varchar",
+                },
+                {
+                    name:"password",
+                    type:"varchar",
+                },
+                {
+                    name:"profileUrl",
+                    type:"varchar"
+                },
+                {
+                    name:"created_at",
+                    type:"timestamp",
+                    default:"now()"
+                }
+            ]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users")
+    }
+
+}
