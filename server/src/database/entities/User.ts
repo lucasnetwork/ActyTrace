cf6271f5b8e3bf80515a83bec111bed6b20fd151
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-05-24 20:42:57 -0300

    change User entitie

--- server/src/database/entities/User.ts
+++ server/src/database/entities/User.ts
@@ -4,7 +4,7 @@ import { v4 as uuid } from 'uuid';
 @Entity("users")
 export default class User{
     @PrimaryGeneratedColumn()
-    readonly id:string;
+     id:string;
 
     @Column()
     email:string;
@@ -14,19 +14,4 @@ export default class User{
 
     @Column()
     password:string;
-
-    @Column()
-    profileUrl:string
-
-    @Column()
-    created_at:Date
-    
-    constructor() {;
-        if (!this.id) {
-          this.id = uuid();
-        }
-        if(!this.created_at){
-            this.created_at = new Date()
-        }
-      }
 }
