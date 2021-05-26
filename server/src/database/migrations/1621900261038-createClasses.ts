ca848d19965c4f44123a46d8c0720018e84cafcf
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-05-26 20:29:13 -0300

    add createClasses migration

---
+++ server/src/database/migrations/1621900261038-createClasses.ts
@@ -0,0 +1,41 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createClasses1621900261038 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name:"classes",
+            columns:[{
+                name:"id",
+                type:"int",
+                isPrimary:true,
+                generationStrategy:"increment"
+            },{
+                name:"name",
+                type:"varchar",
+            },{
+                name:"idTeacher",
+                type:"int",
+            },{
+                name:"idSubject",
+                type:"int"
+            }],
+            foreignKeys:[
+                {
+                    columnNames:["idTeacher"],
+                    referencedColumnNames:["id"],
+                    referencedTableName:"users",
+                },
+                {
+                    columnNames:["idSubject"],
+                    referencedColumnNames:["id"],
+                    referencedTableName:"subjects"
+                }
+            ]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
