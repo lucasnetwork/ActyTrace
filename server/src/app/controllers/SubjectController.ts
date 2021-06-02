17a5f9b2cf188869efad9f2b81f647229e48bd81
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-06-02 20:49:52 -0300

    add SubjectController

---
+++ server/src/app/controllers/SubjectController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import Subject from "../../database/entities/Subject";
+class SubjectController{
+    async create(req:Request,res:Response){
+        const {name,description} = req.body
+        const entityManagerSubject = getRepository(Subject)
+        try{
+            const newSubject = entityManagerSubject.create({name,description})
+            await entityManagerSubject.save(newSubject)
+            return res.status(201).json(newSubject)
+        }catch{
+            return res.status(400).json({error:"error"})
+
+        }
+
+    }
+}
+
+export default SubjectController
