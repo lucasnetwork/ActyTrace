02c65f7c5644d9aa098790cffa701462203a81e5
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-06-02 20:50:26 -0300

    add class and subject routes for create

--- server/src/app/routes.ts
+++ server/src/app/routes.ts
@@ -1,10 +1,16 @@
 import {Router} from 'express'
+import ClassController from './controllers/ClassController'
+import SubjectController from './controllers/SubjectController'
 import UserController from './controllers/UserController'
 
 const userController = new UserController()
+const classController = new ClassController()
+const subjectController = new SubjectController()
 const routes = Router()
 
 routes.post("/user",userController.create)
 routes.get("/user",userController.index)
+routes.post("/class",classController.create)
+routes.post("/subject",subjectController.create)
 
 export default routes
