56d42471088e2886de514e1583361f8ba7205790
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-06-03 20:23:08 -0300

    add sessionController in routes

--- server/src/app/routes.ts
+++ server/src/app/routes.ts
@@ -1,16 +1,19 @@
 import {Router} from 'express'
 import ClassController from './controllers/ClassController'
+import SessionController from './controllers/SessionController'
 import SubjectController from './controllers/SubjectController'
 import UserController from './controllers/UserController'
 
 const userController = new UserController()
 const classController = new ClassController()
 const subjectController = new SubjectController()
+const sessionController = new SessionController()
 const routes = Router()
 
 routes.post("/user",userController.create)
 routes.get("/user",userController.index)
 routes.post("/class",classController.create)
 routes.post("/subject",subjectController.create)
+routes.post("/session",sessionController.create)
 
 export default routes
