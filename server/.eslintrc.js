afeaaaef4e0b39fd2e69cd07da62b69a8ba829bd
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-06-05 21:57:53 -0300

    add jest dependence

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['airbnb-base', 'prettier/@typescript-eslint','plugin:import/typescript'],
+
+  extends: ['airbnb-base', 'prettier', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
@@ -11,20 +12,11 @@ module.exports = {
   },
   plugins: ['@typescript-eslint', 'prettier'],
   rules: {
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
     'linebreak-style': 'off',
     'class-methods-use-this': 'off',
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        ts: 'never',
-      },
-    ],
-    'import/resolver': {
-      node: {
-        extensions: ['.ts', '.jsx'],
-      },
-    },
+    'import/resolver': 'off',
   },
 };
--- server/src/app/controllers/SessionController.ts
+++ server/src/app/controllers/SessionController.ts
@@ -1,36 +1,34 @@
-import bcrypt from 'bcrypt'
-import User from '../../database/entities/User'
-import {Request,Response} from 'express'
-import { getRepository } from 'typeorm'
-import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt';
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import jwt from 'jsonwebtoken';
+import User from '../../database/entities/User';
 
-class SessionController{
-    async create(req:Request,res:Response){
-        const {email,password} = req.body
+class SessionController {
+  async create(req: Request, res: Response) {
+    const { email, password } = req.body;
 
-        const entitieManageUser = getRepository(User)
-        try{
-            const existUser = await entitieManageUser.findOne({email})
+    const entitieManageUser = getRepository(User);
+    try {
+      const existUser = await entitieManageUser.findOne({ email });
 
-            if(!existUser){
-                return res.status(400).json({error:"user not exist"})
-            }
-            
-            const compare = bcrypt.compareSync(password,existUser.password)
-            if(!compare){
-            return res.status(400).json({error:"password error"})
-            }
-            
-            const token = jwt.sign({id:existUser.id},"32132",{
-                expiresIn:60*60*24*10
-            })
-            return res.status(200).json({token})
+      if (!existUser) {
+        return res.status(400).json({ error: 'user not exist' });
+      }
 
-        }catch{
-            return res.status(400).json({error:"user not exist"})
+      const compare = bcrypt.compareSync(password, existUser.password);
+      if (!compare) {
+        return res.status(400).json({ error: 'password error' });
+      }
 
-        }
+      const token = jwt.sign({ id: existUser.id }, '32132', {
+        expiresIn: 60 * 60 * 24 * 10,
+      });
+      return res.status(200).json({ token });
+    } catch {
+      return res.status(400).json({ error: 'user not exist' });
     }
+  }
 }
 
-export default SessionController
+export default SessionController;
