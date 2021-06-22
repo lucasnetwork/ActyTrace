043e2c15ee7dcae62481a069ad560de485d38674
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-06-22 20:42:21 -0300

    add fonts

--- frontend/src/styles/globalStyles.ts
+++ frontend/src/styles/globalStyles.ts
@@ -1,10 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
     *{
         margin:0;
         padding:0;
-        box-sizing:border-box
+        box-sizing:border-box;
+        font-family: 'Roboto', sans-serif;
     }
 
     body,#__next{
